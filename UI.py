fb47a704356296794d85e1198ba79235592c00d1
izaal10 <99273053+izaal10@users.noreply.github.com>
2023-08-08 14:38:48 +0700

    UI.py

--- UI.py
+++ UI.py
@@ -2,7 +2,6 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 def resize_image(image_path, width, height):
-    # Mengubah ukuran gambar sesuai lebar (width) dan tinggi (height) yang diinginkan
     image = Image.open(image_path)
     resized_image = image.resize((width, height), Image.ANTIALIAS)
     return ImageTk.PhotoImage(resized_image)
@@ -14,11 +13,7 @@ def stop_recording():
     status_label.config(text="Record Selesai")
 
 def analyze_emotion():
-    # Fungsi ini akan dijalankan ketika tombol "Ambil Hasil Emosi" ditekan
-    # Di sini Anda dapat menambahkan kode untuk menganalisis hasil emosi
-    # dan menampilkan hasilnya pada label "result_label" dan "suggestion_label"
-
-    # Contoh hasil emosi dan akurasinya
+    # Your existing code for analyzing emotions
     result_emotion = "Senang"
     accuracy = "80%"
 
@@ -28,42 +23,53 @@ def analyze_emotion():
     result_label.config(text=f"Hasil Emosi: {result_emotion} (Akurasi: {accuracy})")
     suggestion_label.config(text=f"Saran: {suggestion}")
 
-# Membuat jendela utama
 root = tk.Tk()
 root.title("YourEmo")
-
-# Mengatur ukuran jendela (lebar x tinggi)
 root.geometry("800x600")
 
-# Membuat label untuk menampilkan gambar
-# Ganti "nama_gambar.png" dengan nama berkas gambar Anda dan pastikan berkas gambar ada dalam direktori yang sama dengan skrip Python Anda.
+# Load the background image
+background_image = Image.open("bg.png")  # Replace with your background image
+background_photo = ImageTk.PhotoImage(background_image)
+
+# Create a Canvas for the background
+canvas = tk.Canvas(root, width=800, height=600)
+canvas.pack()
+
+# Display the background image on the Canvas
+canvas.create_image(0, 0, anchor=tk.NW, image=background_photo)
+
+# Your other widgets
 image_width = 200
 image_height = 170
-image_path = "microphone.png"
+image_path = "record button.png"
 photo = resize_image(image_path, image_width, image_height)
-image_label = tk.Label(root, image=photo)
-image_label.pack(pady=10)
+image_label = tk.Label(canvas, image=photo)
+image_label.place(x=300, y=50)
 
-# Membuat label untuk menampilkan status rekaman
-status_label = tk.Label(root, text="Tidak merekam", font=("Arial", 14))
-status_label.pack()
+status_label = tk.Label(canvas, text="Tidak merekam", font=("Arial", 14))
+status_label.place(x=330, y=250)
 
-# Membuat tombol "Record" dan "Ambil Hasil Emosi"
-record_button = tk.Button(root, text="Record", command=start_recording, font=("Arial", 12))
-record_button.pack(pady=5)
+record_button_image = Image.open("ANALYZE BUTTON.png")  # Replace with your transparent button image
+record_button_photo = ImageTk.PhotoImage(record_button_image)
+record_button = tk.Button(canvas, image=record_button_photo, command=start_recording, bd=0)
+record_button.place(x=300, y=300)
 
-stop_button = tk.Button(root, text="Stop Record", command=stop_recording, font=("Arial", 12))
-stop_button.pack(pady=5)
+# Load the transparent image for the "Stop Record" button
+stop_button_image = Image.open("STOP BUTTON.png")  # Replace with your transparent button image
+stop_button_photo = ImageTk.PhotoImage(stop_button_image)
+stop_button = tk.Button(canvas, image=stop_button_photo, command=stop_recording, bd=0)
+stop_button.place(x=400, y=300)
 
-analyze_button = tk.Button(root, text="Ambil Hasil Emosi", command=analyze_emotion, font=("Arial", 12))
-analyze_button.pack(pady=5)
+# Load the transparent image for the "Analyze" button
+analyze_button_image = Image.open("find BUTTON.png")  # Replace with your transparent button image
+analyze_button_photo = ImageTk.PhotoImage(analyze_button_image)
+analyze_button = tk.Button(canvas, image=analyze_button_photo, command=analyze_emotion, bd=0)
+analyze_button.place(x=300, y=380)
 
-# Membuat label untuk menampilkan hasil emosi dan saran atas hasil emosi
-result_label = tk.Label(root, text="", font=("Arial", 12))
-result_label.pack(pady=10)
+result_label = tk.Label(canvas, text="", font=("Arial", 12))
+result_label.place(x=330, y=470)
 
-suggestion_label = tk.Label(root, text="", font=("Arial", 12))
-suggestion_label.pack(pady=5)
+suggestion_label = tk.Label(canvas, text="", font=("Arial", 12))
+suggestion_label.place(x=330, y=500)
 
-# Menjalankan program
 root.mainloop()
